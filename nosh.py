9804f7d4119253dd128595f7a94d8f616cf36dca
Greg Albrecht <gba@gregalbrecht.com>
2010-09-16 16:56:21 -0700

    Adding info on setting up a Mac OS X Service.

--- nosh.py
+++ nosh.py
@@ -8,9 +8,9 @@ Features
     
 Installation
 ============
-    1) Open nosh.py in your favorite $EDITOR and set splunkServer, username and password.
-    2) Fix nosh.py permissions: chmod +x nosh.py
-    3) Copy nosh.py to a directory in your $PATH
+    1. Open nosh.py in your favorite $EDITOR and set splunkServer, username and password.
+    2. Fix nosh.py permissions: chmod +x nosh.py
+    3. Copy nosh.py to a directory in your $PATH
 
 Usage
 =====
@@ -26,10 +26,24 @@ Usage
     
     ..and open a web browser to the Splunk web interface.
 
+MacOS X Service
+===============
+    nosh.py can be configured as a Mac OS X Service. When enabled, you can use Mac OS X contextual menus 
+    to select and index files using Splunk.
+    
+    To create a custom Mac OS X Service for nosh.py:
+        1. Open Automator.app
+        2. When prompted for a template, select 'Service' (or go to File -> New).
+        3. Set 'Service receives selected' to 'files or folders' in 'any application'.
+        4. In your Actions Library select 'Run Shell Script'.
+        5. In the 'Run Shell Script' action set 'Pass input:' to 'as arguments'.
+        6. For the acutal shell script to run, enter: /path/to/nosh.py $*
+        7. File -> Save and set 'Save Service as:' to 'Index with Splunk'.
+                
 Meta
 ====
     Author: Greg Albrecht (gba@gregalbrecht.com)
-    Version: $Id: //splunk/qa/nosh/nosh.py#4 $
+    Version: $Id: //splunk/qa/nosh/nosh.py#5 $
 """
 ## Change Me!   <----- CHANGE STUFF HERE <------ HEY LOOK AT ME
 splunkServer    = 'localhost'
@@ -41,7 +55,6 @@ import sys
 import socket
 import urllib
 import httplib
-import httplib2
 import logging
 
 from xml.dom import minidom
@@ -56,7 +69,7 @@ splunkServerHTTPPort    =   "8000"
 
 ## Definitely nothing to change here.
 __author__      = "$Author: gba $"
-__version__     = "$Id: //splunk/qa/nosh/nosh.py#4 $"
+__version__     = "$Id: //splunk/qa/nosh/nosh.py#5 $"
 mgmtHostPort    =   ':'.join((splunkServer,splunkServerMgmtPort))
 params          =   { "host" : defaultHost }
 logger          =   logging.getLogger("nosh")
@@ -67,7 +80,7 @@ cliLogger.setLevel(logging.INFO)
 cliLogger.setFormatter(cliLoggerFormat)
 logger.addHandler(cliLogger)
 
-def showResults(searchURL):
+def showResults ( searchURL ) :
     try:
         import webbrowser
         logger.info( "About to open a this URL in a browser: %s" % searchURL )
@@ -75,24 +88,28 @@ def showResults(searchURL):
     except:
         logger.info( "Have at it: %s" % searchURL )
 
-    
-def getSessionKey():
-    serverResponse = httplib2.Http().request( "https://" + mgmtHostPort + '/services/auth/login', 
-        'POST', headers={}, body=urllib.urlencode( { 'username':username, 'password':password } ) )
-    if not serverResponse[0]['status'] == '200':
-        logger.error( "Status did not return 200, perhaps you didn't set your password?" )
-        raise Exception, serverResponse[1]
-    return minidom.parseString(serverResponse[1]).getElementsByTagName('sessionKey')[0].childNodes[0].nodeValue
+
+def getSessionKey ( mgmtHostPort, username, password ):
+    HTTPConn = httplib.HTTPSConnection(mgmtHostPort)
+    HTTPConn.request(   method="POST", url='/services/auth/login', headers={}, 
+                        body=urllib.urlencode( { 'username':username, 'password':password } ) )
+    serverResponse = HTTPConn.getresponse()
+    if not serverResponse.status == 200:
+        logger.error( "Server did not return 200 (it returned '%s %s'). Perhaps you didn't set your password?" % \
+            (serverResponse.status, serverResponse.reason) )
+        raise Exception, ( serverResponse.status, serverResponse.reason )
+    responseConent = serverResponse.read()
+    return minidom.parseString(responseConent).getElementsByTagName('sessionKey')[0].childNodes[0].nodeValue
 
 
-def restBuildEndpoint(params):
+def restBuildEndpoint ( params ):
     endpoint = "/services/receivers/stream"
     if params:
         endpoint += "?" + "&".join(["%s=%s" % (k,v) for k,v in params.items()])
     return endpoint
 
 
-def restUploadFile(dest, endpoint, path, sessionKey):
+def restUploadFile ( dest, endpoint, path, sessionKey ):
     conn = httplib.HTTPSConnection(dest)
     conn.connect()
     conn.putrequest('POST', endpoint)
@@ -118,8 +135,8 @@ def restUploadFile(dest, endpoint, path, sessionKey):
     logger.info( "Uploaded %s" % path )
 
 
-def main():
-    sessionKey = getSessionKey()
+def main ():
+    sessionKey = getSessionKey( mgmtHostPort=mgmtHostPort, username=username, password=password )
     for f in sys.argv[1:]:
         params["source"] = os.path.basename(f)
         restUploadFile( dest=mgmtHostPort, endpoint=restBuildEndpoint(params), path=f, sessionKey=sessionKey )
